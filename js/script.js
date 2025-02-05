b9d97f0c1276712975507fbe72ff6f248c5d6c8d
Fernando Donato <fernandodr60@gmail.com>
2025-02-05 15:50:50 -0300

    adicionada paginação por slide

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import { SlideNav, Slide } from './slide.js'
 const slide = new SlideNav ('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next')
+slide.addControl();
 
 slide.changeSlide(3)
 slide.activePrevSlide()
